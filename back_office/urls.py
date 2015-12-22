d887d05d2445b78573902605f8038cbd6761dbab
Emad Mokhtar <emad.m.habib@gmail.com>
2015-12-22 12:49:08 +0300

    Make model_new is the name convention for create new model url

--- back_office/urls.py
+++ back_office/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^teacher/$', views.TeacherList.as_view(), name='teacher_list'),
-    url(r'^teacher/new/$', views.TeacherCreation.as_view(), name='teacher_creation'),
+    url(r'^teacher/new/$', views.TeacherCreation.as_view(), name='teacher_new'),
     url(r'^teacher/details/(?P<pk>\d+)$', views.TeacherUpdate.as_view(), name='teacher_details'),
     url(r'class-type/$', views.ClassTypeList.as_view(), name='class_type_list'),
     url(r'class-type/new/$', views.ClassTypeCreation.as_view(), name='class_type_new'),
