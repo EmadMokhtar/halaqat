0b6bdffae6e552c9558b0549b4e43fe2c8656774
Emad Mokhtar <emad.m.habib@gmail.com>
2015-12-29 14:05:54 +0300

    Add more tests for teacher model

--- back_office/tests_models.py
+++ back_office/tests_models.py
@@ -1,4 +1,6 @@
 from decimal import Decimal
+
+from django.core.exceptions import ValidationError
 from django.db import IntegrityError
 from django.test import TestCase
 from django.contrib.auth.models import User
@@ -9,7 +11,7 @@ class ClassTypeTestCases(TestCase):
     """
     Testing Class Type model
     """
-    
+
     def setUp(self):
         self.class_type, created = ClassType.objects.get_or_create(name='Class Type 1',
                                                                    monthly_fees=Decimal(12))
@@ -45,12 +47,12 @@ class TeacherTestCases(TestCase):
     """
 
     def setUp(self):
-        self.user = User.objects.create_user('user', 'user@company.com', 'password')
-        self.teacher, created = Teacher.objects.get_or_create(user=self.user, gender='M')
+        self.user = User.objects.create_user('user', 'user@company.com', 'password', first_name='first_name')
+        self.teacher, created = Teacher.objects.get_or_create(user=self.user, gender='M', civil_id='123123')
 
     def test_creating_new_teacher(self):
         new_user = User.objects.create_user('new_teacher', 'user@company.com', 'password')
-        new_teacher = Teacher(user=new_user, gender='M')
+        new_teacher = Teacher(user=new_user, gender='M', civil_id='34534')
         new_teacher.save()
 
         self.assertIsInstance(new_teacher, Teacher)
@@ -66,3 +68,18 @@ class TeacherTestCases(TestCase):
     def test_deleting_teacher_should_disable_not_deleting(self):
         self.teacher.delete()
         self.assertFalse(self.teacher.user.is_active)
+
+    def test_getting_teacher_full_name(self):
+        self.assertNotEqual(self.teacher.full_name, self.user.username)
+        self.assertEqual(self.teacher.full_name, self.user.first_name)
+
+    def test_getting_teacher_name_without_name(self):
+        self.user.first_name = ''
+        self.assertEqual(self.teacher.full_name, self.user.username)
+        self.assertNotEqual(self.teacher.full_name, self.user.first_name)
+
+    def test_create_duplicate_teacher_with_name_and_civilid(self):
+        new_user = User.objects.create_user('new_teacher', 'user@company.com', 'password', first_name='first_name')
+        new_teacher = Teacher(user=new_user, gender='M', civil_id='123123')
+
+        self.assertRaises(ValidationError, lambda :new_teacher.save())
