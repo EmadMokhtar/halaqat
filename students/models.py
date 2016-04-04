01f35ceaa962a7d2509fe4e27f7604a4d0e90d07
Emad Mokhtar <emad.m.habib@gmail.com>
2016-04-04 10:24:30 +0200

    Create Studnets app to hold anything related to Sutdent data and add Student model to it

---
+++ students/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from master_data.models import Nationality
+from back_office.models import Class
+
+PENDING = 'P'
+ACTIVE = 'A'
+SUSPENDED = 'S'
+ON_LEAVE = 'L'
+
+STATUS_CHOICES = (
+    (PENDING, _('Pending')),
+    (ACTIVE, _('Active')),
+    (SUSPENDED, _('Suspended')),
+    (ON_LEAVE, _('On Leave')),
+    )
+
+class Student(models.Model):
+    """
+    Halaqat Student
+    """
+    name = models.CharField(max_length=25, verbose_name=_('Name'))
+    dob = models.DateField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES,
+                              default='M', verbose_name=_('Gender'))
+    civil_id = models.CharField(max_length=12, unique=True,
+                                verbose_name=_('Civil ID'))
+    mobile_number = models.CharField(max_length=12, verbose_name=_('Mobile'))
+    home_number = models.CharField(max_length=12, verbose_name=_('Home'))
+    parent_number = models.CharField(max_length=12, verbose_name=_('Parents'))
+    grade = models.CharField(max_length=5, verbose_name=_('Grade'))
+    school = models.CharField(max_length=12, verbose_name=_('School'))
+    nationality = models.ForeignKey(Nationality, verbose_name=_('Nationality'))
+    address = models.CharField(max_length=150, verbose_name=_('Address'))
+    email = models.EmailField(verbose_name=_('Email'))
+    parent_email = models.EmailField(verbose_name=_('Parent Email'))
+    halaqat_class = models.ForeignKey(Class)
+    enrollment_date = models.DateField(verbose_name=_('Enrollment Date'))
+    old_enrollment_date = models.DateField(verbose_name=_('Pervious Center Enrollment Date'))
+    chapter_memeorized = models.IntegerField(verbose_name=_('chapter Memeorized'))
+    chapter_memeorized_with_center = models.IntegerField(verbose_name=_('chapters memeorized with center'))
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES,
+                              default=PENDING,
+                              verbose_name=_('Status'))
