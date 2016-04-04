4d9fb9e1b69d8d411571f4656ef79913bba2a1bc
Emad Mokhtar <emad.m.habib@gmail.com>
2016-04-04 10:31:20 +0200

    Make initial migration

---
+++ master_data/migrations/0001_initial.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Nationality',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=10, verbose_name='Name')),
+                ('is_active', models.BooleanField(default=True, verbose_name='Active')),
+            ],
+        ),
+    ]
