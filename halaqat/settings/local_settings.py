d410fb26d3fb8bbd843234e90891bee5a5fff7e7
Emad Mokhtar <emad.m.habib@gmail.com>
2016-12-17 18:32:36 +0300

    Add local dev settings module

---
+++ halaqat/settings/local_settings.py
@@ -0,0 +1,17 @@
+from .base_settings import *
+
+DEBUG = True
+
+LANGUAGE_CODE = 'en'
+
+TIME_FORMAT = [
+
+    '%I:%M %p',
+    '%H:%M %p',
+]
+
+
+TIME_INPUT_FORMATS = [
+    '%I:%M %p',
+    '%H:%M %p'
+]
