0eb4ee77adbecff3a6b08e9597f284f7488f7015
Emad Mokhtar <emad.m.habib@gmail.com>
2017-01-05 15:33:34 +0300

    Herokuify the application

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn halaqat.wsgi --log-file -
---
+++ halaqat/settings/shaha.py
@@ -0,0 +1,26 @@
+from .base_settings import *
+import dj_database_url
+import os
+
+ALLOWED_HOSTS = ['0.0.0.0']
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
