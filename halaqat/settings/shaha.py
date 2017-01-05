233ce96d96caff3070f24d9d3dff3ed85be81fee
Emad Mokhtar <emad.m.habib@gmail.com>
2017-01-05 16:37:28 +0300

    Fix The STATICFILES_DIRS setting should not contain the STATIC_ROOT setting

--- halaqat/settings/shaha.py
+++ halaqat/settings/shaha.py
@@ -15,11 +15,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
