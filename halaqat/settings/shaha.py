2a8fa1e2a6f73d8f84008968e13378494b8a2e78
Emad Mokhtar <emad.m.habib@gmail.com>
2017-01-13 00:15:47 +0300

    Update the settings file

--- halaqat/settings/shaha.py
+++ halaqat/settings/shaha.py
@@ -7,16 +7,11 @@ ALLOWED_HOSTS = ['shaha-halaqat.herokuapp.com', '0.0.0.0']
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
+STATICFILES_DIRS = ()
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
@@ -46,3 +41,6 @@ TEMPLATES = [
 ]
 
 LANGUAGE_CODE = 'ar'
+LOCALE_PATHS = (
+    os.path.join(PROJECT_ROOT, 'locale'),
+)
