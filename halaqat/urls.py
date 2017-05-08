41b36817225cad3303c05b91f8b86f36c1734eec
Emad Mokhtar <emad.m.habib@gmail.com>
2017-05-08 11:08:39 +0300

    Change logout to redirect the user to home page

--- halaqat/urls.py
+++ halaqat/urls.py
@@ -10,10 +10,9 @@ from .views import IndexView
 urlpatterns = i18n_patterns(
     url(r'^$', view=IndexView.as_view(), name='home'),
     url(r'^login/$', auth_views.login,
-        {'template_name': 'login.html'},
-        name='login'),
+        {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout,
-        {'next_page': '/login'}, name='logout'),
+        {'next_page': '/'}, name='logout'),
     url(r'^back-office/', include(back_office_url)),
     url(r'^back-office/students/', include(students_url)),
     url(r'^admin/', include(admin.site.urls)),
